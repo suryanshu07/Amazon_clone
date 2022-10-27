f457b28e3d17dd607415543f4bbceee1568bfa5d
Suryanshu Gautam <58790023+suryanshu07@users.noreply.github.com>
2022-10-27 12:12:16 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDnZBRrUUPMFHDWX4tkOpSE4AM_l1I5yJY",
+    authDomain: "react--clone-308b0.firebaseapp.com",
+    projectId: "react--clone-308b0",
+    storageBucket: "react--clone-308b0.appspot.com",
+    messagingSenderId: "825237206073",
+    appId: "1:825237206073:web:005d541c156e4d74a4c9e8",
+    measurementId: "G-1MR4QZ8CRN"
+  };
